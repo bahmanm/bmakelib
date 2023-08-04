0d5da9e47d240a4f16956538326293e33ad935cd
Bahman Movaqar <bahman@bahmanm.com>
2023-08-04 07:23:57 -0700

    partially break down runner into runner_lib.sh

---
+++ tests/runner_lib.sh
@@ -0,0 +1,49 @@
+####################################################################################################
+# echo.error MSG
+####################################################################################################
+function echo.error {
+  echo "$@" >&2
+}
+
+####################################################################################################
+# pushd DIR
+####################################################################################################
+function pushd {
+  command pushd "$@" > /dev/null
+}
+
+####################################################################################################
+# popd DIR
+####################################################################################################
+function popd {
+  command popd "$@" > /dev/null
+}
+
+####################################################################################################
+# run_a_test_suite TEST_SUITE
+#
+# Globals:
+#   target_dir
+#   root_dir
+####################################################################################################
+function run_a_test_suite {
+  perl -E 'say ">" x 80'
+
+  local test_file="${root_dir}${1}"
+  local test_name="$(basename $test_file)"
+  local test_dir="${target_dir}/${test_name}"
+
+  echo "runner: Running ${test_name} in ${test_dir}..."
+  mkdir -p $test_dir || echo.error "runner: Could not create ${test_dir}."
+  cp $test_file $test_dir || echo.error  "runner: Could not find ${test_file}."
+  pushd $test_dir
+  if ! ./${test_name}; then
+    echo.error "runner: 🔴 ${test_name} failed."
+    popd
+    perl -E 'say "<" x 80'
+    return 1
+  fi
+  popd
+
+  perl -E 'say "<" x 80'
+}
